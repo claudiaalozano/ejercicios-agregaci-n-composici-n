a2da57c1415ebda9a3e418f879af89f0404d3fa6
claudiaalozano <clau.lozano.perez@gmail.com>
2022-03-25 16:55:16 +0100

    creo la clase edificio

--- Clases/ejercicio1.py
+++ Clases/ejercicio1.py
@@ -6,16 +6,29 @@ class ciudad:
         self.newyork = newyork
         self.angeles = angeles
     def set_Newyork (self, newyork, seleccion_ciudad):
-        self.newyork = newyork
         if seleccion_ciudad == "NY":
             print("Se ha eliminado New York.")
             del(newyork)
-    def getNewyork (self):
+    def get_newyork (self):
         print( set.newyork)
 
 
-    def angeles (self, angeles, seleccion_ciudad):
-        self.angeles = angeles
+    def set_angeles (self, angeles, seleccion_ciudad):
+        
         if seleccion_ciudad == "L.A":
             print("Se ha eliminado Los Angeles.")
-        
+            del(angeles)
+    def get_angeles(self):
+        print(set.angeles)
+
+
+class edificio:
+    def __init__(self , a, b , c):
+        self.a = a
+        self.b = b
+        self.c = c
+    def set_a(self,newyork):
+        if del(newyork):
+            del(a)
+            
+

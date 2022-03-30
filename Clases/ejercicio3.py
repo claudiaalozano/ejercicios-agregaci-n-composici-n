8a8f0659a16db6e33464f9d0f537a55a0929fab1
claudiaalozano <clau.lozano.perez@gmail.com>
2022-03-30 14:15:15 +0200

    creo la variable ejercicio en el main

--- Clases/ejercicio3.py
+++ Clases/ejercicio3.py
@@ -1,9 +1,12 @@
-class Pared:
+class Cristal: #clase padre
+    pass
+
+class Pared(Cristal): #clase hija
     #Constructor
     def __init__(self, orientacion):
         self.orientacion = orientacion
         
-class Ventana(Pared):
+class Ventana(Cristal): #clase hija
     #Constructor
     def __init__(self, orientacion, superficie):
         super().__init__(orientacion)
@@ -12,7 +15,7 @@ class Ventana(Pared):
     def get_area(self):
         return self.superficie
 
-class Casa(Ventana):
+class Casa(Cristal): #clase hija
     #Constructor
     def __init__(self, paredes, orientacion, superficie):
         super().__init__(orientacion, superficie)
@@ -20,3 +23,18 @@ class Casa(Ventana):
     
     def superficie_acristalada(self):
         return sum(self.paredes.superficie)
+
+
+
+pared_norte = Pared("NORTE") 
+pared_oeste = Pared("OESTE") 
+pared_sur = Pared("SUR") 
+pared_este = Pared("ESTE") 
+    # Instanciación de las ventanas 
+ventana_norte = Ventana(pared_norte, 0.5) 
+ventana_oeste = Ventana(pared_oeste, 1) 
+ventana_sur = Ventana(pared_sur, 2) 
+ventana_este = Ventana(pared_este, 1) 
+    # Instanciación de la casa con las 4 paredes 
+casa = Casa([pared_norte, pared_oeste, pared_sur, pared_este]) 
+    #print(casa.superficie_acristalada()) 
---
+++ main.py
@@ -0,0 +1,2 @@
+ejercicio = int(input("Seleccione el ejercicio que desea ejecutar (1,2 o 3): "))
+

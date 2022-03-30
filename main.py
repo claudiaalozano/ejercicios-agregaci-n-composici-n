3e7660d9e49c27819354ad92ba7604f2f16fb3a6
claudiaalozano <clau.lozano.perez@gmail.com>
2022-03-30 23:23:51 +0200

    main para ejercicio 1 y 2

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from Clases import *
 if ejercicio == "1":
     print("Ejercicio 1: ")
     from Clases.ejercicio1 import *
-    
+    ciudad1 = ciudad(seleccion_ciudad)
+    print(ciudad1)
 
-if ejercicio == "2":
+elif ejercicio == "2":
+    print("Ejercicio 2: ")
+    from Clases.ejercicio2 import *
+    Yang1 =  Yang
+    print(Yang1)
 
 if ejercicio == "3":
     Cristal1 = 
